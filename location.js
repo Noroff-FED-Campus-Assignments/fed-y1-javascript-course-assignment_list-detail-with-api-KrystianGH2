6935a2ad3b90f368ab8ee8af5cfa7ec8a7f283cf
KrystianGH2 <kgcdronio@gmail.com>
2023-02-24 15:34:26 +0100

    Rick&morty first commit

--- location.js
+++ location.js
@@ -1,45 +1,74 @@
 
-const urlLocation =  "https://rickandmortyapi.com/api/location/"
-const ramCharacterHolder = document.querySelector(".ram-character-container")
+
+const urlLocation =  "https://rickandmortyapi.com/api/location/?page=5"
+const locationContainer = document.querySelector(".location-card-info")
+
+
 
 async function fetchLocation() {
-
-    try{
-
-        const response = await fetch (urlLocation);
-
-        // console.log(response);
-
-        const location = await response.json();
-
-        console.log(location.results);
-
-        let locationData = location.results
-
-        ramCharacterHolder.innerHTML = "";
-
-        locationData.forEach(location => {
-
-            ramCharacterHolder.innerHTML += `
-            <div class="cardHolder">
-            <span>${location.name}</span> 
-            <span>${location.dimension}</span>
-            <span>${location.type}</span>
-
-
-            </div>
-            `
-
-
-
-        })
-
-
+    
+        try{
         
+                const response = await fetch (urlLocation);
+        
+                // console.log(response);
+        
+                const location = await response.json();
+        
+                // console.log(location.results);
+        
+                let locationData = location.results
+        
+                locationContainer.innerHTML = "";
+            
+            locationData.forEach(location => {
+        
+                        locationContainer.innerHTML +=  `
+            <div class="box-parent"> 
+                    <div class="box">
+                        <h5 class="h5-location-name"> ${location.name}</h5>
+                        <span><i class="fa-solid fa-angle-down"></i></span>
+                    </div>
+                    <div class="card-box animate__animated animate__fadeInRight">
+                        <div class="card-info-elements">Dimension: ${location.dimension}</div>
+                        <div class="card-info-elements">Type: ${location.type}</div>
+                        <div class="card-info-elements">ID: ${location.id}</div>
+                    </div>
+            </div>`   
 
-    } catch(error) {
-        console.log(error)
-    }
-}
+                })       
+                
+                const boxParent = document.querySelectorAll('.box-parent')
 
-fetchLocation()
+                boxParent.forEach(boxes => {
+                    const cardBox = boxes.querySelector('.card-box')
+                    boxes.addEventListener('click', () =>{
+                        cardBox.classList.toggle('card-box-show')
+                    })
+
+                })
+                
+                
+        
+            } catch(error) {
+                    console.log(error)        
+                }        
+            }        
+            
+            fetchLocation()
+
+            
+            
+            
+            
+            
+            const dropDownBtn = document.querySelector('.drop-down-nav');
+            const navList = document.querySelector(".nav-list");
+            dropDownBtn.addEventListener('click', () => {
+                setTimeout(() =>  { 
+                    navList.classList.toggle('nav-list-show') + dropDownBtn.classList.toggle('drop-down-rotate')   
+                    ;},200)   
+                });     
+                
+                
+                
--- main.js
+++ main.js
@@ -1,17 +1,26 @@
-
 const ramCharacterHolder =  document.querySelector(".ram-character-container");
 const input = document.querySelector("#input");
-// const getApiBtn = document.querySelector(".getApi")
-const url = "https://rickandmortyapi.com/api/character/?page=33";
+const url = "https://rickandmortyapi.com/api/character/?page=16";
+
+
+// ui js
+const dropDownBtn = document.querySelector('.drop-down-nav');
+const navList = document.querySelector(".nav-list");
+ dropDownBtn.addEventListener('click', () => {
+    setTimeout(() =>  { 
+     navList.classList.toggle('nav-list-show') + dropDownBtn.classList.toggle('drop-down-rotate')
+     ;},200)   
+});
 
 
 
 //Search bar on keyup event
 input.addEventListener("keyup", (e) => {
     const searchValue = e.target.value.trim().toLowerCase();
+    console.log(searchValue)
     fetchCharacters(searchValue)
-    
 })
+
 
 
 async function fetchCharacters(searchValue = "")  {
@@ -28,41 +37,50 @@ async function fetchCharacters(searchValue = "")  {
         const  filteredCharacters = characters.filter(function(characters){
             return characters.name.toLowerCase().includes(searchValue)
         })
-        
-        
+
         ramCharacterHolder.innerHTML = "";
-      
+
         
         filteredCharacters.forEach(characters => {
             ramCharacterHolder.innerHTML += `
-            <a href= "details.html?id=${characters.id} & ${characters.name} ">
+            <a class= "a-holder" href= "details.html?id=${characters.id} & ${characters.name} ">
             <div class="cardHolder">
-            <img src="${characters.image}" alt="${characters.name} " />
+            <img class ="character-image" src="${characters.image}" alt="${characters.name} " />
             <div class="cardInfo">
-            <h4> ${characters.name}</h4>
+            <h5 class = "h4-character-names"> ${characters.name}</h5>
             <div class = "span-status-gender">
             <span class= "span1"> ${characters.status}</span><span> - ${characters.species}</span>  
             </div>
-            <p> ${characters.gender}</p>
-            <p> ${characters.origin.name}</p> 
-            <p> ${characters.id}</p> 
+            <p class ="p-character-cardInfo"> Origin: ${characters.origin.name}</p> 
             </div> 
             </div></a> 
             
             `
+            
         });
-
-        
         
     } catch (err) {
         console.log(err);
     }
-    
-    
 }
-// getApiBtn.addEventListener('click', () => {
-    //     fetchCharacters();
-    // })
-    
-    fetchCharacters();
-    
+
+fetchCharacters();
+
+
+// image slideshow carousel
+
+let images = ['src/image/image0.avif','src/image/image1.webp','src/image/image2.png','src/image/image3.webp']
+const carousel = document.querySelector('#images-slideshow');
+let i = 0;
+function slideShow() {
+        carousel.src=images[i];
+        if(i < images.length-1) {
+                i++;
+            } else{
+                    i = 0;
+                }
+                setTimeout("slideShow()",5000)
+            }
+            window.onload = slideShow;
+            
+            
